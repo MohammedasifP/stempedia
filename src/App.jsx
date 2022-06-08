98859b24a6259a57cf280296bb274d30bbaa6b11
Mohammedasif Pinitod <87132170+MohammedasifP@users.noreply.github.com>
2022-06-08 12:43:34 +0530

    task 3 completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Componenta} from './components/componentA/ComponentA'
+
+function App() {
+  return (
+    <div>
+      <Componenta />
+    </div>
+  );
+}
+
+export default App;
